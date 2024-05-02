be8ba285a6257fda8037f1b26e4245f74b775b07
vitorhugoutida <vitorhugoutida@gmail.com>
2024-05-02 17:26:49 -0300

    feat: Exercicio 1 feito

--- exemplo.js
+++ exemplo.js
@@ -1,19 +1,29 @@
-// // Escreva um programa que solicite o nome do usuario e lhe dê boas vindas,
-// // exibindo uma mensagem pelo console.
+// // // Escreva um programa que solicite o nome do usuario e lhe dê boas vindas,
+// // // exibindo uma mensagem pelo console.
 
-// // prompt -> Recebe uma entrada digitada
+// // // prompt -> Recebe uma entrada digitada
 
-// let nome = prompt("Olá, digite seu nome:")
+// // let nome = prompt("Olá, digite seu nome:")
 
-// // Concatenação
+// // // Concatenação
 
-// console.log("Seja bem vindo " + nome + "!!!");
+// // console.log("Seja bem vindo " + nome + "!!!");
 
 
 let pontos = 100
 
-// parsenInt() -> Transforma o que esta entre () em numero inteiro.
+// // parsenInt() -> Transforma o que esta entre () em numero inteiro.
 
-let bonus = parseInt( prompt("Quantos pontos você deseja somar?") )
+// let bonus = parseInt( prompt("Quantos pontos você deseja somar?") )
 
-console.log(pontos + bonus)
+// Adiacao -> num1 + num2
+// Substracao -> num1 - num2
+// multiplicacao -> num1 * num2
+// Divisao -> num1 / num2
+// console.log(pontos + bonus)x'
+
+// // parseFloat() -> Transforma o que esta entre () em numero com , (quebrado)
+
+let notaAluno = parseFloat(prompt("Digite a nota do aluno: "))
+
+console.log(notaAluno)
