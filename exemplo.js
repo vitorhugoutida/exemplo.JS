96c96526621c1bc503d9f60e632a2c69ab5deec2
vitorhugoutida <vitorhugoutida@gmail.com>
2024-05-02 17:01:55 -0300

    feat: Exemplo

---
+++ exemplo.js
@@ -0,0 +1,19 @@
+// // Escreva um programa que solicite o nome do usuario e lhe dê boas vindas,
+// // exibindo uma mensagem pelo console.
+
+// // prompt -> Recebe uma entrada digitada
+
+// let nome = prompt("Olá, digite seu nome:")
+
+// // Concatenação
+
+// console.log("Seja bem vindo " + nome + "!!!");
+
+
+let pontos = 100
+
+// parsenInt() -> Transforma o que esta entre () em numero inteiro.
+
+let bonus = parseInt( prompt("Quantos pontos você deseja somar?") )
+
+console.log(pontos + bonus)
